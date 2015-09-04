412de6e93b6df6aecab1e41ebd6cf8204a71200e
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2015-09-04 11:43:43 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyanno",
+    version = "0.1a",
+    author = "Farnaz",
+    author_email = "farnazh@gmail.com",
+    description = "PyAnno annotating things and labeling things and sorting things ",
+    license = "BSD",
+    keywords = "",
+    url = "https://github.com/FarnazH/pyanno_voting.git",
+    packages=['pyanno'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
